b1aae3a5d70d56c931b0c37994ed38ae3e94c67d
ZhengX-94 <xanderxu1994@gmail.com>
2020-06-30 19:43:31 -0400

    added SEO analyzer for selected specified website

---
+++ marketwebapp/seo/python_seo_analyzer/seoanalyzer/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+from .analyzer import analyze
+from .stemmer import stem
+from seoanalyzer import *
--- marketwebapp/seo/views.py
+++ marketwebapp/seo/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
 
+# from python_seo_analyzer.seoanalyzer.analyzer import analyze
+from seoanalyzer import analyzer
+
+
 def index(request):
-    return render(request, "seo/index.html")
+    if request.method == "GET":
+        return render(request, "seo/index.html")
+    if request.method == "POST":
+        website = request.POST["websitetoseo"]
+        print("query is: ", website)
+        output = analyzer.analyze(website)
+        print(output["pages"][0]["warnings"])
+        return render(request, "seo/result.html", {"result": output})
+
